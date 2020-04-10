499a7c213202388fa659e864f4c543f14717990d
PawelPruchniak <pw.pruchniak@gmail.com>
2020-04-10 13:53:09 +0200

    Create fragments, and basic nav_graph.

---
+++ app/src/main/java/com/example/sklepallegro/detail/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.sklepallegro.detail
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/sklepallegro/overview/OverviewViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.sklepallegro.overview
+
+import androidx.lifecycle.ViewModel
+
+class OverviewViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
